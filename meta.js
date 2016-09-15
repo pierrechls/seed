541d355134d75da5135c3723c9cf194927f4bc77
Pierre Charles <pierre@soixantecircuits.fr>
2016-09-15 15:01:44 +0200

    Add template

---
+++ meta.js
@@ -0,0 +1,34 @@
+module.exports = {
+  prompts: {
+    name: {
+      type: 'string',
+      required: true,
+      message: 'Project name'
+    },
+    description: {
+      type: 'string',
+      required: false,
+      message: 'Project description',
+      default: 'A vue-cli project'
+    },
+    author: {
+      type: 'string',
+      message: 'Author'
+    },
+    electron: {
+      type: 'confirm',
+      message: 'Setup Electron support (might break browser support)?'
+    }
+  },
+  filters: {
+    'electron/**/*': 'electron'
+  },
+  helpers: {
+    chandeDirectory: () => {
+      return (process.argv[3] === undefined || process.argv[3] === '.')
+        ? ''
+        : `  cd ${process.argv[3]}\n`
+    }
+  },
+  completeMessage: 'To get started:\n\n{{chandeDirectory}}  npm install\n  npm start\n\nDocumentation can be found at https://pierrechls.github.io/vue-template'
+}
---
+++ template/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <menu></menu>
+    <p>Welcome!</p>
+    <p><a href="http://webpack.github.io/" target="_blank">Webpack</a> documentation.</p>
+    <p><a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a> documentation.</p>
+    <router-view transition="default"></router-view>
+  </div>
+</template>
+
+<script>
+import store from './vuex/store'
+import Menu from './components/Menu.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Menu
+  },
+  route: {
+    data: function (transition) {
+      // console.log('App.vue - transition from %s to %s', transition.from.name, transition.to.name)
+    }
+  },
+  store
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+  position: relative;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+#app > div {
+  background: #EEE;
+  position: absolute;
+  width: 100%;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
