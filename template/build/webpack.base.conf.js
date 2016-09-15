ff8f26f5dd001c33eba94f603a138d339610e9a5
Pierre Charles <pierre@soixantecircuits.fr>
2016-09-15 17:35:27 +0200

    Add settings folder

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -29,6 +29,7 @@ module.exports = {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
       'components': path.resolve(__dirname, '../src/components'),
+      'settings': path.resolve(__dirname, '../settings'),
       'locales': path.resolve(__dirname, '../locales'),
       'data': path.resolve(__dirname, '../src/data'),
       'lib': path.resolve(__dirname, '../src/lib')
