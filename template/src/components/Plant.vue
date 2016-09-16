b67650520654fa03962109eb8ac2f30696574535
Pierre Charles <pierre@soixantecircuits.fr>
2016-09-16 14:24:33 +0200

    Update plant view

--- template/src/components/Plant.vue
+++ template/src/components/Plant.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="plant-view">
     <h1>Plant</h1>
-    <p>\{{ seedNumber }} <span v-if="seedNumber > 1">seeds</span><span v-else>seed</span> are planted</p>
+    <p>\{{ seedNumber }} <span v-if="seedNumber > 1">\{{{ $t('plant.seedText') }}}</span><span v-else>\{{{ $t('plant.seedsText') }}}</span></p>
     <button @click='plant' :disabled="seedNumber <= minSeed">-</button>
     <button @click='pullUp' :disabled="seedNumber >= maxSeed">+</button>
   </div>
