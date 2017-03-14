d8748817ec3c4aedba74a40ab1b9097bfaa6c61d
Pierre Charles <pierre.chls@gmail.com>
2017-03-14 20:13:00 +0100

    :tada: Add deploy doc script

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -1,8 +1,8 @@
-cd doc
+cd documentation
 rm -rf _book
 gitbook build
 cd _book
 git init
 git add -A
-git commit -m ":sparkles: Update doc"
+git commit -m ":sparkles: Update documentation"
 git push -f git@github.com:pierrechls/seed.git master:gh-pages
